bd706f4c25a27ecc1896cd9f8a2b463906c2e104
yokatsu <wassabi1002@gmail.com>
2021-08-28 13:51:48 -0300

    colocando as promoções

--- produto.php
+++ produto.php
@@ -9,53 +9,66 @@
    </head>
   
 	<body>
-	  <header>
-		<div class= "caixa">
-			<h1><img src ="imagens/logo.png"></h1>
-			
-		<nav>
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="produto.php">Produtos</a></li>
-				<li><a href="contato.php">Contato</a></li>
-		
+	  	<header>
+			<div class= "caixa">
+				<h1><img src ="imagens/logo.png"></h1>
+				<nav>
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="produto.php">Produtos</a></li>
+						<li><a href="contato.php">Contato</a></li>
+					</ul>
+				</nav>
+			</div>
+    	</header>
+	
+		<main>
+			<ul class = "produtos">
+				<li>
+					<h2>Cabelo</h2>
+					<img src ="imagens/cabelo.jpg">
+					<p class = "produto-descricao">Na tesoura ou máquina, como o cliente preferir.</p>
+					<p class = "produto-preco">R$25,OO</p>
+				</li>
+				<li>
+					<h2>Barba</h2>
+					<img src ="imagens/barba.jpg">
+					<p class = "produto-descricao">Corte e desenho profissional de barba.</p>
+					<p class = "produto-preco">R$18,00</p>
+				</li>
+				<li>
+					<h2>Cabelo + barba</h2>
+					<img src ="imagens/cabelo+barba.jpg">
+					<p class = "produto-descricao">Pacote completo de cabelo e barba.</p>
+					<p class = "produto-preco">R$35,00</p>				
+				</li>
 			</ul>
-		</nav>
-	</div>
-    </header>
-	
-	<main>
-		<ul class = "produtos">
-			<li>
-				<h2>Cabelo</h2>
-				<img src ="imagens/cabelo.jpg">
-				<p class = "produto-descricao">Na tesoura ou máquina, como o cliente preferir.</p>
-				<p class = "produto-preco">R$25,OO</p>
-				
-			</li>
-			<li>
-				<h2>Barba</h2>
-				<img src ="imagens/barba.jpg">
-				<p class = "produto-descricao">Corte e desenho profissional de barba.</p>
-				<p class = "produto-preco">R$18,00</p>
-				
-			</li>
-			<li>
-			<h2>Cabelo + barba</h2>
-				<img src ="imagens/cabelo+barba.jpg">
-				<p class = "produto-descricao">Pacote completo de cabelo e barba.</p>
-				<p class = "produto-preco">R$35,00</p>
-				
-			</li>
-		</ul>
-	</main>
-	
-	<footer>
-    <img src="imagens/logo-branco.png">
-    <p class="copyright">&copy; Copyright Barbearia Alura - 2021</p>
-</footer>
-	
+			<h2>Promoções</h2>
+			<ul class = "produtos">
+				<li>
+					<h2>Cabelo</h2>
+					<img src ="imagens/cabelo.jpg">
+					<p class = "produto-descricao">Na tesoura ou máquina, como o cliente preferir.</p>
+					<p class = "produto-preco">R$25,OO</p>
+				</li>
+				<li>
+					<h2>Barba</h2>
+					<img src ="imagens/barba.jpg">
+					<p class = "produto-descricao">Corte e desenho profissional de barba.</p>
+					<p class = "produto-preco">R$18,00</p>
+				</li>
+				<li>
+					<h2>Cabelo + barba</h2>
+					<img src ="imagens/cabelo+barba.jpg">
+					<p class = "produto-descricao">Pacote completo de cabelo e barba.</p>
+					<p class = "produto-preco">R$35,00</p>				
+				</li>
+			</ul>
+		</main>
+
+		<footer>
+    		<img src="imagens/logo-branco.png">
+    		<p class="copyright">&copy; Copyright Barbearia Alura - 2021</p>
+		</footer>
 	</body>
-
-
 </html>
