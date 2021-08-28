d1427ba7e2738bc1e6a21f0cffd6c137bfae8884
yokatsu <wassabi1002@gmail.com>
2021-08-28 14:00:49 -0300

    adicionei promoções

--- produto.php
+++ produto.php
@@ -48,11 +48,35 @@
 				
 			</li>
 		</ul>
+		<h2>Promoção do dia</h2>
+		<ul class = "produtos">
+			<li>
+				<h2>Cabelo</h2>
+				<img src ="imagens/cabelo.jpg">
+				<p class = "produto-descricao">Na tesoura ou máquina, como o cliente preferir.</p>
+				<p class = "produto-preco">R$15,OO</p>
+				
+			</li>
+			<li>
+				<h2>Barba</h2>
+				<img src ="imagens/barba.jpg">
+				<p class = "produto-descricao">Corte e desenho profissional de barba.</p>
+				<p class = "produto-preco">R$8,00</p>
+				
+			</li>
+			<li>
+			<h2>Cabelo + barba</h2>
+				<img src ="imagens/cabelo+barba.jpg">
+				<p class = "produto-descricao">Pacote completo de cabelo e barba.</p>
+				<p class = "produto-preco">R$25,00</p>
+				
+			</li>
+		</ul>
 	</main>
 	
 	<footer>
-    <img src="imagens/logo-branco.png">
-    <p class="copyright">&copy; Copyright Barbearia Alura - 2021</p>
+    	<img src="imagens/logo-branco.png">
+    	<p class="copyright">&copy; Copyright Barbearia Alura - 2021</p>
 	</footer>
 	
 	</body>
