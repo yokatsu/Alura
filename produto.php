dea89001cf34a71423dd00e6fa5510896c8acce2
yokatsu <wassabi1002@gmail.com>
2021-08-28 13:39:00 -0300

    testando o php

--- produto.php
+++ produto.php
@@ -15,7 +15,7 @@
 			
 		<nav>
 			<ul>
-				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php">Home<?php echo("home");?></a></li>
 				<li><a href="produto.php">Produtos</a></li>
 				<li><a href="contato.php">Contato</a></li>
 		
